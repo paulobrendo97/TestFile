09205a5392fa211ff3e2f0d11836da84dfb91372
Paulo Brendo <paulobrendo016@icloud.com>
2025-04-02 20:48:21 -0300

    teste 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,24 @@
 package application;
 
-public class Program {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Program {
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\Suke\\eclipse-workspace\\TestFile\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
 }
